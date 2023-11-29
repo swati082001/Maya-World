91f9487a239fe9ac314a79976c57fbd9a34c534b
swati082001 <swatisucharitamohanty1@gmail.com>
2023-11-29 12:37:54 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1100, // Set your desired limit in kilobytes
+  },
   plugins: [react()],
   assetsInclude:['**/*.glb'],
   base:"/mayan-world/"
